e77b3141f1cf961d52353fe6831ef8ff4dc88af3
tommylogon <tommy.slaatbraaten@hotmail.com>
2018-11-13 19:52:04 +0100

    Merge branch 'Unification'

---
+++ LearnThaiApplication/Classes/UserSetting.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnThaiApplication.Classes
+{
+    public class UserSetting
+    {
+        public bool DescriptionOn { get; set; }
+        public bool LoopChapter { get; set; }
+        public bool DisplayAllPropertiesInDescription { get; set; }
+        public bool RandomOn { get; set; }
+        public bool SkipIntro { get; set; }
+        public string WhatToDisplay { get; set; }
+        public string WhatToTrain { get; set; }
+
+        public UserSetting(bool descriptionOn, bool loopChapter, bool displayAllPropertiesInDescription, bool randomOn, bool skipIntro, string whatToDisplay, string whatToTrain)
+        {
+            DescriptionOn = descriptionOn;
+            LoopChapter = loopChapter;
+            DisplayAllPropertiesInDescription = displayAllPropertiesInDescription;
+            RandomOn = randomOn;
+            SkipIntro = skipIntro;
+            WhatToDisplay = whatToDisplay;
+            WhatToTrain = whatToTrain;
+        }
+
+        public UserSetting()
+        {
+        }
+    }
+}
